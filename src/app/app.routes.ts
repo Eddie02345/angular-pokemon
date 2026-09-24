0fa432288389a8c3d70ee74d44f52d546746c2d3
aquino_e <aquino_e@f213.ub.lan>
2026-09-24 08:55:18 +0800

    Remove mention of hoenn easter egg on why is it still not there

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,6 @@ import { HomeComponent } from './components/home/home';
 import { Kanto } from './components/kanto/kanto';
 import { Johto } from './components/johto/johto';
 
-// TODO: Route '/hoenn' is temporarily disabled.
-// Celio's Network Machine in the Sevii Islands still needs the Ruby and Sapphire plates. (Lore drop lmao)
-// Tanong lang sir, sino starter nyo sa gen 3 (Treecko, Torchic, or Mudkip)? 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
